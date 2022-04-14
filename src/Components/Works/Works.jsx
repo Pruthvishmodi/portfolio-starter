037bd75a2a14d6bd9543eed1c6008a838237655e
pruthvish modi <iampruthvishmodi@gmail.com>
2022-04-14 12:51:45 +0530

    work rightside parth working

--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./Works.css";
+import Upwork from "../../img/Upwork.png";
+import Fiverr from "../../img/fiverr.png";
+import Amazon from "../../img/amazon.png";
+import Shopify from "../../img/Shopify.png";
+import Facebook from "../../img/Facebook.png";
 
 export const Work = () => {
   return (
@@ -9,11 +14,9 @@ export const Work = () => {
         <span>Work For All These</span>
         <span>Brands & Clients</span>
         <span>
-          I am a Front-End developer and love working with new technology.{" "}
-          <br /> I do development to deliver a great end-to-end solution focused
-          on user experience.
-          <br /> My main focus these days is SEO, performance, type system, and
-          code architecture.
+          Work with local, as well as, international clients has become an
+          amazing opportunity that I don't take for granted. Take a look at my
+          latest projects that I’ve been busy working on.
         </span>
 
         <button className="button s-button">Hire Me</button>
@@ -22,6 +25,29 @@ export const Work = () => {
           style={{ background: "var(--blueCard)" }}
         ></div>
       </div>
+      {/*right side path */}
+      <div className="w-right">
+        <div className="w-maincircle">
+          <div className="w-secCircle">
+            <img src={Upwork} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Fiverr} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Amazon} alt="" />
+          </div>{" "}
+          <div className="w-secCircle">
+            <img src={Shopify} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Facebook} alt="" />
+          </div>
+        </div>
+      </div>
+      {/*background parth */}
+      <div className="w-backCircle bluCircle"></div>
+      <div className="w-backCircle yellowCircle"></div>
     </div>
   );
 };
